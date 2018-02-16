f1891c430ae4bedf284cedc676ee5359a1f31159
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-02-16 16:55:43 -0500

    [DESIGN] Polymorphisme du code Roues

--- src/org/firstchampionship/equipe5910/robot2018/Robot.java
+++ src/org/firstchampionship/equipe5910/robot2018/Robot.java
@@ -3,12 +3,13 @@ package org.firstchampionship.equipe5910.robot2018;
 import org.firstchampionship.equipe5910.robot2018.soussysteme.Bras;
 import org.firstchampionship.equipe5910.robot2018.soussysteme.Chariot;
 import org.firstchampionship.equipe5910.robot2018.soussysteme.Pince;
+import org.firstchampionship.equipe5910.robot2018.soussysteme.Roues;
 import org.firstchampionship.equipe5910.robot2018.soussysteme.RouesTankDrive;
 
 public class Robot {
 	
 	//public static Roues roues;
-	public static RouesTankDrive roues;
+	public static Roues roues;
 	public static Chariot chariot;
 	public static Pince pince;
 	public static Bras bras;
