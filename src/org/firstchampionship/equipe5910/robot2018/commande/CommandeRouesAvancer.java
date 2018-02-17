ccb2845adff1a2918c4283211cef3d383ecf3cac
nadineducegep <nadineducegep@gmail.com>
2018-02-17 17:45:54 -0500

    [TASK] Solution de detection d'immobilite Factorisee - desactivee en attendant tests

--- src/org/firstchampionship/equipe5910/robot2018/commande/CommandeRouesAvancer.java
+++ src/org/firstchampionship/equipe5910/robot2018/commande/CommandeRouesAvancer.java
@@ -1,55 +1,29 @@
 package org.firstchampionship.equipe5910.robot2018.commande;
 
-import java.util.concurrent.SynchronousQueue;
-
 import org.firstchampionship.equipe5910.robot2018.Robot;
 import org.firstchampionship.equipe5910.robot2018.RobotMap;
-
-import edu.wpi.first.wpilibj.Timer;
+import org.firstchampionship.equipe5910.robot2018.outil.InterpreteurMouvement;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class CommandeRouesAvancer extends Command{
-
-	public static int PAUSE_TOLEREE = 1000;
-	public static double DISTANCE_NULLE = 50; // TODO peut-être un ratio sur la distance desiree !
-	public static int ITERATIONS_PAR_MILLISECONDE = 50/1000;
 	
 	protected double distanceVoulue;
 	protected double positionInitiale = 0;
 	protected double vitesse = 0.1; // test primitif
 	protected boolean PIDFirstLoop = true;
+	protected InterpreteurMouvement verificateurImmobilite;
 	
-	protected double pauseToleree = PAUSE_TOLEREE; // en millisecondes
-	protected int iterationsImmobile = 1;
-	// protected Timer horloge = null;// TODO version avec timer
-	protected SynchronousQueue<Double> dernieresPositions = new SynchronousQueue<Double>();
-
 	public CommandeRouesAvancer(double distanceVoulue)
 	{
 		requires(Robot.roues);		
 		this.distanceVoulue = distanceVoulue;
 		//this.horloge = new Timer();
-		this.calculeIterationsImmobiles();
+		verificateurImmobilite = new InterpreteurMouvement();
 	}
 
 	public void setPauseToleree(double pauseToleree)
 	{
-		this.pauseToleree = pauseToleree;
-		this.calculeIterationsImmobiles();
-	}
-	
-	private void calculeIterationsImmobiles()
-	{
-		this.iterationsImmobile = (int) (pauseToleree * 50 / 1000);
-		for(int position = 0; position < this.iterationsImmobile; position++)
-		{
-			try {
-				this.dernieresPositions.put(new Double(0));
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		
+		this.verificateurImmobilite.setPauseToleree(pauseToleree);
 	}
 	
 	@Override
@@ -64,23 +38,10 @@ public class CommandeRouesAvancer extends Command{
 		// Robot.roues.avancer(vitesse); 
 		// Robot.roues.informerEncodeurDroitDeLaCible(distanceVoulue);
 	}
-
-	protected double distanceActuelle;
-	protected double distanceAncienne;
 	
 	@Override
 	protected void execute() {
-		
-		try {
-			//double temps = this.horloge.get();
-			distanceActuelle = Robot.roues.getDistanceGauche();
-			this.dernieresPositions.put(new Double(distanceActuelle));
-			distanceAncienne = this.dernieresPositions.remove();
-			
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
+				
 		if (distanceVoulue - Robot.roues.getDistanceGauche() <= 500)
 		{
 			if (PIDFirstLoop)
@@ -100,17 +61,18 @@ public class CommandeRouesAvancer extends Command{
 		//Robot.roues.avancer(vitesse); 
 		//Robot.roues.avancer(Robot.roues.getVitesseSelonEncodeurDroitStabilise()); 
 		// System.out.println("selon pid vitesse = "  + Robot.roues.getVitesseSelonEncodeurDroitStabilise());
+		
+		this.verificateurImmobilite.mesurer();
 	}
 	
 	protected boolean estArrive = false;
-	protected boolean estImmobile = false;
 	@Override
 	protected boolean isFinished() {
 		System.out.println("Distance parcourue " + (Robot.roues.getDistanceGauche() - this.positionInitiale));
 		estArrive = Robot.roues.getDistanceGauche() >= (this.distanceVoulue);
-		estImmobile = false; //estImmobile = ((distanceActuelle - distanceAncienne) < this.DISTANCE_NULLE);
-		System.out.println("estArrive " + estArrive + " est immobile " + estImmobile);
-		return estArrive || estImmobile;
+		System.out.println("estArrive " + estArrive + " est immobile " + this.verificateurImmobilite.estImmobile());
+		return estArrive;
+		//return estArrive || this.verificateurImmobilite.estImmobile();
 	}
 	
 	@Override
---
+++ src/org/firstchampionship/equipe5910/robot2018/outil/InterpreteurMouvement.java
@@ -0,0 +1,60 @@
+package org.firstchampionship.equipe5910.robot2018.outil;
+
+import java.util.concurrent.SynchronousQueue;
+
+import org.firstchampionship.equipe5910.robot2018.Robot;
+
+public class InterpreteurMouvement {
+
+	public static int PAUSE_TOLEREE = 1000;
+	public static double DISTANCE_NULLE = 50; // TODO peut-être un ratio sur la distance desiree !
+	public static int ITERATIONS_PAR_MILLISECONDE = 50/1000;
+	protected double pauseToleree = PAUSE_TOLEREE; // en millisecondes
+	protected int iterationsImmobile = 1;
+	// protected Timer horloge = null;// TODO version avec timer
+	protected SynchronousQueue<Double> dernieresPositions = new SynchronousQueue<Double>();
+
+	public InterpreteurMouvement()
+	{
+		this.calculerIterationsImmobiles();
+	}
+	
+	public void setPauseToleree(double pauseToleree)
+	{
+		this.pauseToleree = pauseToleree;
+		this.calculerIterationsImmobiles();
+	}
+	
+	private void calculerIterationsImmobiles()
+	{
+		this.iterationsImmobile = (int) (pauseToleree * 50 / 1000);
+		for(int position = 0; position < this.iterationsImmobile; position++)
+		{
+			try {
+				this.dernieresPositions.put(new Double(0));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+	
+	protected double distanceActuelle;
+	protected double distanceAncienne;
+	public void mesurer()
+	{
+		try {
+			//double temps = this.horloge.get();
+			distanceActuelle = Robot.roues.getDistanceGauche();
+			this.dernieresPositions.put(new Double(distanceActuelle));
+			distanceAncienne = this.dernieresPositions.remove();
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean estImmobile()
+	{
+		return ((distanceActuelle - distanceAncienne) < this.DISTANCE_NULLE);
+	}
+}
