0b58cd5aa89a5fb644958df63df76e482d271143
nadineducegep <nadineducegep@gmail.com>
2018-02-16 22:26:41 -0500

    Lecteur des attributions de Plate - version avec objet sans enum

--- src/org/firstchampionship/equipe5910/robot2018/interaction/LecteurAttributionsAutonomes.java
+++ src/org/firstchampionship/equipe5910/robot2018/interaction/LecteurAttributionsAutonomes.java
@@ -3,7 +3,17 @@ package org.firstchampionship.equipe5910.robot2018.interaction;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class LecteurAttributionsAutonomes {
-					
+			
+	public class Attribution
+	{
+		public char premier;
+		public char second;
+		public char troisieme;
+	}
+		
+	Attribution attribution = new Attribution();
+	//char attributions[] = new char[3];
+		
 	public void lire()
 	{
 		String message = DriverStation.getInstance().getGameSpecificMessage();
@@ -11,24 +21,25 @@ public class LecteurAttributionsAutonomes {
 		
 		if(message.length() > 0)
 		{
-			char premier = message.charAt(0);
-			char second = message.charAt(1);
-			char troisieme = message.charAt(2);
+			attribution.premier = message.charAt(0);
+			attribution.second = message.charAt(1);
+			attribution.troisieme = message.charAt(2);
 			
-			System.out.println("premier : " + premier);
-			System.out.println("second : " + second);
-			System.out.println("troisieme : " + troisieme);			
+			System.out.println("premier : " + attribution.premier);
+			System.out.println("second : " + attribution.second);
+			System.out.println("troisieme : " + attribution.troisieme);			
 		}
 		
 	}
 	
-	/*public void vider()
+	public void vider()
 	{
 		String message;
 		do
 		{
 			message = DriverStation.getInstance().getGameSpecificMessage();
 		}
-		while(message.length() > 0);		
-	}*/
+		while(message.length() > 0);
+		
+	}
 }
